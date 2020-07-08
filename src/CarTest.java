e3275dbec82dc010cbe40bd22e642d6d38ad691e
ETFund <be.rich.nomad@gmail.com>
2020-07-08 17:28:21 +0900

    copy constructor

--- src/CarTest.java
+++ src/CarTest.java
@@ -6,8 +6,13 @@ class Car{
     Car() {
         this("white", "auto", 4);
     }
-    Car(String color){
-        this (color, "auto", 4);
+    Car(Car c){
+        /*
+        color = c.color;
+        gearType = c.gearType;
+        door = c.door;
+         */
+        this(c.color, c.gearType, c.door);
     }
     Car(String color, String gearType, int door){
         this.color = color;
@@ -19,8 +24,13 @@ class Car{
 public class CarTest {
     public static void main(String[] args){
         Car c1 = new Car();
-        Car c2 = new Car("blue", "auto", 4);
+        Car c2 = new Car(c1); // c1의 복사본 c3를 생성
 
+        System.out.println("c1의 color = " + c1.color + ", gearType = " + c1.gearType + ", door = " + c1.door);
+        System.out.println("c2의 color = " + c2.color + ", gearType = " + c2.gearType + ", door = " + c2.door);
+
+        c1.door = 100; // c1의 인스턴스 변수 door 값을 변경
+        System.out.println("c1.door = 100; 수행 후");
         System.out.println("c1의 color = " + c1.color + ", gearType = " + c1.gearType + ", door = " + c1.door);
         System.out.println("c2의 color = " + c2.color + ", gearType = " + c2.gearType + ", door = " + c2.door);
     }
